c92d582fb96828ca4d0e99c2f75fa95e66418a9d
ranJAn <57830610+aadesh-coder@users.noreply.github.com>
2022-10-17 03:12:39 +0530

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,8 @@
+This quiz website project is made by Aadesh Ranjan Srivastav alone.
+
+HTML, CSS and Javascript is used in building the project.
+
+To start the project just click on the index.html file.
+
+
+heroku website link : - 
---
+++ js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following keywords is used to define a variable in Javascript?",
+    answer: "Both A and B",
+    options: [
+      "var",
+      "let",
+      "Both A and B",
+      "None of the Above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following methods can be used to display data in some form using Javascript?",
+    answer: "All of the above",
+    options: [
+      "document.write()",
+      "console.log()",
+      "window.alert()",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What keyword is used to check whether a given property is valid or not?",
+    answer: "in",
+    options: [
+      "in",
+      "is in",
+      "exists",
+      "lies"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the use of the <noscript> tag in Javascript?",
+    answer: "The contents are displayed by non-JS-based browsers.",
+    options: [
+      "The contents are displayed by non-JS-based browsers.",
+      "Clear all the cookies and cache.",
+      "Both A and B.",
+      "None of the above."
+    ]
+  },
+    {
+    numb: 5,
+    question: "Javascript is an _______ language?",
+    answer: "Object-Oriented",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Which function is used to serialize an object into a JSON string in Javascript?",
+    answer: "stringify()",
+    options: [
+      "stringify()",
+      "parse()",
+      "convert()",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 7,
+    question: "JavaScript code can be written in ____.",
+    answer: "JavaScript file and in HTML document directly",
+    options: [
+      "JavaScript file (.js file)",
+      "JavaScript file and in HTML document directly",
+      "In style sheets (.css file)",
+      "HTML document directly"
+    ]
+  },
+  {
+    numb: 8,
+    question: "In JavaScript, what do we use for calling the expression for function definition?",
+    answer: "Function literal",
+    options: [
+      "Function literal",
+      "Function prototype",
+      "Function declaration",
+      "Function calling"
+    ]
+  },
+  {
+    numb: 9,
+    question: "In JavaScript, single line comment begins with ___.",
+    answer: "//",
+    options: [
+      "#",
+      "/*",
+      "$",
+      "//"
+    ]
+  },
+  {
+    numb: 10,
+    question: "The 'function' and 'var' are known as:",
+    answer: "Declaration Statements",
+    options: [
+      "Keywords",
+      "Data Types",
+      "Declaration Statements",
+      "Prototypes"
+    ]
+  }
+];
